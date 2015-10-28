d27dfa1a0861282a48ed62cc36a6a1c2366b4d84
ChristianRapp <christian.rapp@hotmail.com>
2015-10-28 14:48:59 -0600

    Initial Commit

---
+++ Chess/src/ChessMain.java
@@ -0,0 +1,13 @@
+
+public class ChessMain
+	{
+
+		public static void main(String[] args)
+			{
+				
+				
+				
+
+			}
+
+	}
---
+++ Chess/src/Pieces.java
@@ -0,0 +1,22 @@
+
+public class Pieces
+	{
+
+		protected String piece;
+		protected int xCoord;
+		protected int yCoor;
+		
+		
+		
+		public String move(String m)
+		{
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
